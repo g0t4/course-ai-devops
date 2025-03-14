c150f78f55911947d834bedd35a42edcfc01d804
Wes Higbee <wes.mcclure@gmail.com>
2025-03-14 14:17:18 -0500

    debug

--- orders/server.py
+++ orders/server.py
@@ -76,4 +76,4 @@ def page_not_found(_):
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
